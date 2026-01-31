68b1ee64baef6ca5970860491ac667ad24b53cc7
“Nazar <3016976B@student.gla.ac.uk>
2026-01-31 15:33:46 +0000

    Completed chapter 4 all test pass

--- rango/views.py
+++ rango/views.py
@@ -1,17 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# def index(request):
+def index(request):
 
-#     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
 
-#     return render(request, 'rango/index.html', context=context_dict)
+    return render(request, 'rango/index.html', context=context_dict)
 
-
-def index(requst):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
-
-
-def about(request): 
-    
-    return HttpResponse('Rango says here is the about page. <a href="/rango/">Index</a>' ) 
+def about(request):
+    return render(request, 'rango/about.html')
